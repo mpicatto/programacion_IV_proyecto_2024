564e3e7a82bef2482bbcf5d7821b9b852e72cae3
Mauro Picatto <mauropicatto@gmail.com>
2024-05-06 08:58:26 -0300

    Se agrega archivo main.py se me expande readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # programacion_IV_proyecto_2024
 Proyecto Integrador  to Año
 Tema: Sistema de Accesso mediante reconocimiento Facial
+
+
+Estructura del Proyecto
+
+-carpeta modules: Dentro de estar carpeta vamos a crear los modulos necesarios para las diferentes partes del proyecto
+
+main.py: Archivo de entrada del proyecto. Importa todos los modulos creados  en el proyecto  y módulos de terceros que sean necesarios. 
---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Este Archivo es la puerta de entrada al proyecto. Se ejecuta el proyecto con el comando python main.py.
+En Este archivo se van a ir importrando todos los modulos de terceros  y los módulos creados por nosotros
+'''
+
+
+'''
+Seccion 1 Importaciones. En este espacio se haran todas las importaciones
+'''
+
+
+
+'''
+Seccion 2 Definir Clases, Funciones 
+'''
+
+
+
+'''
+Seccion 3 Definir VAriables, llamar Funciones e Instanciar Clases
+'''
